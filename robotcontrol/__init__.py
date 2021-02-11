1e80c0f4dadfd90ebec2dc35fcc47cc611ceabee
Zinc-OS <63823426+Zinc-OS@users.noreply.github.com>
2021-02-11 11:13:20 -0500

    Update __init__.py

--- robotcontrol/__init__.py
+++ robotcontrol/__init__.py
@@ -100,8 +100,8 @@ class RobotControlPlugin(octoprint.plugin.SettingsPlugin,
 					self._logger.error("%s", e)
 			#return None
 			#time.sleep(1)
-		else:
-			return gcode
+		
+		return gcode
             			
 	@octoprint.plugin.BlueprintPlugin.route("/servo1", methods=["GET"])
 	@restricted_access
